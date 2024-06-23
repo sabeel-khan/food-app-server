b2456045b6344973dbc6f780beeb05e2d09e42bc
Sabeel Khan <sabeel.khan@paysense.in>
2024-06-23 14:02:16 +0530

    whitlisted netlify FE url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://dashing-bunny-4ecabf.netlify.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true
 }));
